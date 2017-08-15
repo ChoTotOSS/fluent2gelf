1ddff69b6f68ad73e99eb9d75629503bb18fae23
Thinh Tran <duythinht@gmail.com>
2017-08-15 18:28:17 +0700

    Add sample run

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
+import (
+	"flag"
+	"net"
+	"os"
+
+	"go.uber.org/zap"
+
+	"github.com/ChoTotOSS/fluent2gelf/agent"
+	"github.com/ChoTotOSS/fluent2gelf/fluentd"
+	"github.com/duythinht/zaptor"
+)
+
+var logger = zaptor.Default()
+
 func main() {
-	a := agent.New()
+
+	var c string
+
+	flag.StringVar(&c, "c", "sample.yml", "config file")
+	flag.Parse()
+
+	f, _ := os.Open(c)
+	agentStore := agent.AgentStoreLoad(f)
+
+	doneList := make([](chan bool), len(agentStore.AgentList))
+
+	for i, agent := range agentStore.AgentList {
+		done := make(chan bool)
+		doneList[i] = done
+		go agent.Run(done)
+	}
+
+	serv, err := net.Listen("tcp", ":24224")
+	checkError(err)
+
+	for {
+		conn, err := serv.Accept()
+		logger.Debug("New client connected", zap.Any("conn", conn))
+		checkError(err)
+		go fluentd.ForwardHandle(conn, agentStore)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		logger.Fatal("main#main", zap.Error(err))
+		os.Exit(1)
+	}
 }
